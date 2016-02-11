818fe2bd78d570963869c7eacb24c277d322d6cc
Simon Pratt <simon@pr4tt.com>
2016-02-11 09:16:33 -0500

    Now globbing all pgn files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate glob;
+use glob::glob;
+
+//use std::error::Error;
+//use std::fs::File;
+//use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    for entry in glob("data/*.pgn").unwrap() {
+        match entry {
+            Ok(path) => load_pgn(path.as_path()),
+            Err(e)   => println!("{:?}", e),
+        }
+    }
+}
+
+fn load_pgn(path: &Path) {
+    println!("{:?}", path.display());
 }
