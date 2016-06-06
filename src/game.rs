bf04da69e01162cb3200dbd2ce09c3fd825a3512
Simon Pratt <Simon.Pratt@uwaterloo.ca>
2016-06-06 17:43:18 -0400

    load_pgn -> Game::from_pgn

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,10 @@
 extern crate unicode_segmentation;
 use game::unicode_segmentation::UnicodeSegmentation;
 
+use std::path::Path;
 use std::collections::HashMap;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
 
 pub struct Results {
     white: u64,
@@ -79,5 +82,40 @@ impl Game {
             tagpairs: tp.clone(),
             results: Game::parse_results(&tp),
         }
+    }
+
+    pub fn from_pgn(path: &Path) -> Vec<Game> {
+        let mut v = Vec::new();
+        let display = path.display();
+        let file = match File::open(path) {
+            Ok(f) => f,
+            _     => panic!("opening {}", display),
+        };
+        let reader = BufReader::new(file);
+        let mut header = Vec::new();
+        let mut moves = Vec::new();
+        let mut reading_header = true;
+        for l in reader.lines() {
+            let line = match l {
+                Ok(ln) => ln,
+                _      => panic!("reading {}", display),
+            };
+            if line == "" {
+                reading_header = !reading_header;
+                if reading_header {
+                    v.push(Game::new(&header, &moves));
+                    header = Vec::new();
+                    moves = Vec::new();
+                }
+                continue;
+            }
+            if reading_header {
+                header.push(line);
+            } else {
+                moves.push(line);
+            }
+        }
+        println!("{:?}", display); // remove in future
+        v
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 extern crate glob;
 use glob::glob;
 
-use std::io::{BufRead, BufReader};
-use std::fs::File;
-use std::path::Path;
-
 mod game;
 use game::Game;
 
@@ -12,43 +8,8 @@ fn main() {
     let mut v = Vec::new();
     for entry in glob("data/*.pgn").unwrap() {
         match entry {
-            Ok(path) => v.extend(load_pgn(path.as_path()).iter().cloned()),
+            Ok(path) => v.extend(Game::from_pgn(path.as_path()).iter().cloned()),
             Err(e)   => println!("{:?}", e),
         }
     }
-}
-
-fn load_pgn(path: &Path) -> Vec<Game> {
-    let mut v = Vec::new();
-    let display = path.display();
-    let file = match File::open(path) {
-        Ok(f) => f,
-        _     => panic!("opening {}", display),
-    };
-    let reader = BufReader::new(file);
-    let mut header = Vec::new();
-    let mut moves = Vec::new();
-    let mut reading_header = true;
-    for l in reader.lines() {
-        let line = match l {
-            Ok(ln) => ln,
-            _      => panic!("reading {}", display),
-        };
-        if line == "" {
-            reading_header = !reading_header;
-            if reading_header {
-                v.push(Game::new(&header, &moves));
-                header = Vec::new();
-                moves = Vec::new();
-            }
-            continue;
-        }
-        if reading_header {
-            header.push(line);
-        } else {
-            moves.push(line);
-        }
-    }
-    println!("{:?}", display); // remove in future
-    v
 }
